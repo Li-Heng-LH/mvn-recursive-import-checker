e4d24c0a2a42899b9f24f760b05953003d99c451
Li Heng <liheng6567@gmail.com>
2020-08-12 17:48:46 +0800

    Initial set up

---
+++ src/test/java/me/liheng/importchecker/AppTest.java
@@ -0,0 +1,13 @@
+package me.liheng.importchecker;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
